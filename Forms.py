8f020fb7a20d1183cb90821ba3876f8b67d40574
Vitor Noguchi <vshojifn@gmail.com>
2020-04-22 19:18:35 -0300

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForms(FlaskForm):
     username = StringField('Username',
                             validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Sign Up')
 
 class AttemptForms(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     number = StringField('Attempt', validators=[DataRequired(), Length(min=4, max=4)])
     submit = SubmitField('Try')
